344bf21967c40f21524d733e6cc01094b90d7fe4
mkonic <mkonic676@gmail.com>
2026-09-12 03:05:58 +0300

    debug: add no-net flag to skip the online hooks

--- src/core/hooking/Hooking.cpp
+++ src/core/hooking/Hooking.cpp
@@ -55,36 +55,41 @@ namespace YimMenu
 		BaseHook::Add<Hooks::Script::ScriptVM>(new DetourHook("ScriptVM", Pointers.ScriptVM, Hooks::Script::ScriptVM));
 		BaseHook::Add<Hooks::Script::RegisterCompappNatives>(new DetourHook("RegisterCompappNatives", Pointers.RegisterCompappNatives, Hooks::Script::RegisterCompappNatives));
 
-		BaseHook::Add<Hooks::Protections::HandleNetGameEvent>(new DetourHook("HandleNetGameEvent", Pointers.HandleNetGameEvent, Hooks::Protections::HandleNetGameEvent));
-		BaseHook::Add<Hooks::Protections::HandleCloneCreate>(new DetourHook("HandleCloneCreate", Pointers.HandleCloneCreate, Hooks::Protections::HandleCloneCreate));
-		BaseHook::Add<Hooks::Protections::HandleCloneSync>(new DetourHook("HandleCloneSync", Pointers.HandleCloneSync, Hooks::Protections::HandleCloneSync));
-		BaseHook::Add<Hooks::Protections::GetCloneCreateResponse>(new DetourHook("GetCloneCreateResponse", Pointers.GetCloneCreateResponse, Hooks::Protections::GetCloneCreateResponse));
-		BaseHook::Add<Hooks::Protections::CanApplyData>(new DetourHook("CanApplyData", Pointers.CanApplyData, Hooks::Protections::CanApplyData));
-		BaseHook::Add<Hooks::Protections::ResetSyncNodes>(new DetourHook("ResetSyncNodes", Pointers.ResetSyncNodes, Hooks::Protections::ResetSyncNodes));
-		BaseHook::Add<Hooks::Protections::HandleScriptedGameEvent>(new DetourHook("HandleScriptedGameEvent", Pointers.HandleScriptedGameEvent, Hooks::Protections::HandleScriptedGameEvent));
-		BaseHook::Add<Hooks::Protections::AddObjectToCreationQueue>(new DetourHook("AddObjectToCreationQueue", Pointers.AddObjectToCreationQueue, Hooks::Protections::AddObjectToCreationQueue));
-		BaseHook::Add<Hooks::Protections::ReceiveNetMessage>(new DetourHook("ReceiveNetMessage", Pointers.ReceiveNetMessage, Hooks::Protections::ReceiveNetMessage));
-		BaseHook::Add<Hooks::Protections::HandlePresenceEvent>(new DetourHook("HandlePresenceEvent", Pointers.HandlePresenceEvent, Hooks::Protections::HandlePresenceEvent));
-		BaseHook::Add<Hooks::Protections::PPostMessage>(new DetourHook("PostMessage", Pointers.PostPresenceMessage, Hooks::Protections::PPostMessage));
-		BaseHook::Add<Hooks::Protections::SerializeServerRPC>(new DetourHook("SerializeServerRPC", Pointers.SerializeServerRPC, Hooks::Protections::SerializeServerRPC));
-		BaseHook::Add<Hooks::Protections::ReceiveServerMessage>(new DetourHook("ReceiveServerMessage", Pointers.ReceiveServerMessage, Hooks::Protections::ReceiveServerMessage));
-		BaseHook::Add<Hooks::Protections::DeserializeServerMessage>(new DetourHook("DeserializeServerMessage", (void*)((__int64)GetModuleHandleA(0) + 0x2609f78), Hooks::Protections::DeserializeServerMessage));
-		BaseHook::Add<Hooks::Protections::ReceiveArrayUpdate>(new DetourHook("ReceiveArrayUpdate", Pointers.ReceiveArrayUpdate, Hooks::Protections::ReceiveArrayUpdate));
-		BaseHook::Add<Hooks::Protections::CreatePoolItem>(new DetourHook("CreatePoolItem", Pointers.CreatePoolItem, Hooks::Protections::CreatePoolItem));
-		BaseHook::Add<Hooks::Protections::HandleCloneRemove>(new DetourHook("HandleCloneRemove", Pointers.HandleCloneRemove, Hooks::Protections::HandleCloneRemove));
-		BaseHook::Add<Hooks::Protections::PackCloneCreate>(new DetourHook("PackCloneCreate", Pointers.PackCloneCreate, Hooks::Protections::PackCloneCreate));
-		BaseHook::Add<Hooks::Protections::SetTreeErrored>(new DetourHook("SetTreeErrored", Pointers.SetTreeErrored, Hooks::Protections::SetTreeErrored));
-		BaseHook::Add<Hooks::Protections::PhysicsHandleLassoAttachment>(new DetourHook("PhysicsHandleLassoAttachment", Pointers.PhysicsHandleLassoAttachment, Hooks::Protections::PhysicsHandleLassoAttachment));
-		BaseHook::Add<Hooks::Protections::DecideConnectionMethod>(new DetourHook("DecideConnectionMethod", Pointers.DecideConnectionMethod, Hooks::Protections::DecideConnectionMethod));
-		BaseHook::Add<Hooks::Protections::HandlePeerRelayPacket>(new DetourHook("HandlePeerRelayPacket", Pointers.HandlePeerRelayPacket, Hooks::Protections::HandlePeerRelayPacket));
-		BaseHook::Add<Hooks::Protections::UnpackPacket>(new DetourHook("UnpackPacket", Pointers.UnpackPacket, Hooks::Protections::UnpackPacket));
-		BaseHook::Add<Hooks::Protections::UpdateEndpointAddress>(new DetourHook("UpdateEndpointAddress", Pointers.UpdateEndpointAddress, Hooks::Protections::UpdateEndpointAddress));
-		BaseHook::Add<Hooks::Protections::CanCreateNetworkObject>(new DetourHook("CanCreateNetworkObject", Pointers.CanCreateNetworkObject, Hooks::Protections::CanCreateNetworkObject));
+		if (TestFlag("no-net"))
+			LOG(WARNING) << "testflags: not hooking online code";
+		else
+		{
+			BaseHook::Add<Hooks::Protections::HandleNetGameEvent>(new DetourHook("HandleNetGameEvent", Pointers.HandleNetGameEvent, Hooks::Protections::HandleNetGameEvent));
+			BaseHook::Add<Hooks::Protections::HandleCloneCreate>(new DetourHook("HandleCloneCreate", Pointers.HandleCloneCreate, Hooks::Protections::HandleCloneCreate));
+			BaseHook::Add<Hooks::Protections::HandleCloneSync>(new DetourHook("HandleCloneSync", Pointers.HandleCloneSync, Hooks::Protections::HandleCloneSync));
+			BaseHook::Add<Hooks::Protections::GetCloneCreateResponse>(new DetourHook("GetCloneCreateResponse", Pointers.GetCloneCreateResponse, Hooks::Protections::GetCloneCreateResponse));
+			BaseHook::Add<Hooks::Protections::CanApplyData>(new DetourHook("CanApplyData", Pointers.CanApplyData, Hooks::Protections::CanApplyData));
+			BaseHook::Add<Hooks::Protections::ResetSyncNodes>(new DetourHook("ResetSyncNodes", Pointers.ResetSyncNodes, Hooks::Protections::ResetSyncNodes));
+			BaseHook::Add<Hooks::Protections::HandleScriptedGameEvent>(new DetourHook("HandleScriptedGameEvent", Pointers.HandleScriptedGameEvent, Hooks::Protections::HandleScriptedGameEvent));
+			BaseHook::Add<Hooks::Protections::AddObjectToCreationQueue>(new DetourHook("AddObjectToCreationQueue", Pointers.AddObjectToCreationQueue, Hooks::Protections::AddObjectToCreationQueue));
+			BaseHook::Add<Hooks::Protections::ReceiveNetMessage>(new DetourHook("ReceiveNetMessage", Pointers.ReceiveNetMessage, Hooks::Protections::ReceiveNetMessage));
+			BaseHook::Add<Hooks::Protections::HandlePresenceEvent>(new DetourHook("HandlePresenceEvent", Pointers.HandlePresenceEvent, Hooks::Protections::HandlePresenceEvent));
+			BaseHook::Add<Hooks::Protections::PPostMessage>(new DetourHook("PostMessage", Pointers.PostPresenceMessage, Hooks::Protections::PPostMessage));
+			BaseHook::Add<Hooks::Protections::SerializeServerRPC>(new DetourHook("SerializeServerRPC", Pointers.SerializeServerRPC, Hooks::Protections::SerializeServerRPC));
+			BaseHook::Add<Hooks::Protections::ReceiveServerMessage>(new DetourHook("ReceiveServerMessage", Pointers.ReceiveServerMessage, Hooks::Protections::ReceiveServerMessage));
+			BaseHook::Add<Hooks::Protections::DeserializeServerMessage>(new DetourHook("DeserializeServerMessage", (void*)((__int64)GetModuleHandleA(0) + 0x2609f78), Hooks::Protections::DeserializeServerMessage));
+			BaseHook::Add<Hooks::Protections::ReceiveArrayUpdate>(new DetourHook("ReceiveArrayUpdate", Pointers.ReceiveArrayUpdate, Hooks::Protections::ReceiveArrayUpdate));
+			BaseHook::Add<Hooks::Protections::CreatePoolItem>(new DetourHook("CreatePoolItem", Pointers.CreatePoolItem, Hooks::Protections::CreatePoolItem));
+			BaseHook::Add<Hooks::Protections::HandleCloneRemove>(new DetourHook("HandleCloneRemove", Pointers.HandleCloneRemove, Hooks::Protections::HandleCloneRemove));
+			BaseHook::Add<Hooks::Protections::PackCloneCreate>(new DetourHook("PackCloneCreate", Pointers.PackCloneCreate, Hooks::Protections::PackCloneCreate));
+			BaseHook::Add<Hooks::Protections::SetTreeErrored>(new DetourHook("SetTreeErrored", Pointers.SetTreeErrored, Hooks::Protections::SetTreeErrored));
+			BaseHook::Add<Hooks::Protections::PhysicsHandleLassoAttachment>(new DetourHook("PhysicsHandleLassoAttachment", Pointers.PhysicsHandleLassoAttachment, Hooks::Protections::PhysicsHandleLassoAttachment));
+			BaseHook::Add<Hooks::Protections::DecideConnectionMethod>(new DetourHook("DecideConnectionMethod", Pointers.DecideConnectionMethod, Hooks::Protections::DecideConnectionMethod));
+			BaseHook::Add<Hooks::Protections::HandlePeerRelayPacket>(new DetourHook("HandlePeerRelayPacket", Pointers.HandlePeerRelayPacket, Hooks::Protections::HandlePeerRelayPacket));
+			BaseHook::Add<Hooks::Protections::UnpackPacket>(new DetourHook("UnpackPacket", Pointers.UnpackPacket, Hooks::Protections::UnpackPacket));
+			BaseHook::Add<Hooks::Protections::UpdateEndpointAddress>(new DetourHook("UpdateEndpointAddress", Pointers.UpdateEndpointAddress, Hooks::Protections::UpdateEndpointAddress));
+			BaseHook::Add<Hooks::Protections::CanCreateNetworkObject>(new DetourHook("CanCreateNetworkObject", Pointers.CanCreateNetworkObject, Hooks::Protections::CanCreateNetworkObject));
 
-		BaseHook::Add<Hooks::Voice::EnumerateAudioDevices>(new DetourHook("EnumerateAudioDevices", Pointers.EnumerateAudioDevices, Hooks::Voice::EnumerateAudioDevices));
-		BaseHook::Add<Hooks::Voice::DirectSoundCaptureCreate>(new DetourHook("DirectSoundCaptureCreate", Pointers.DirectSoundCaptureCreate, Hooks::Voice::DirectSoundCaptureCreate));
-		BaseHook::Add<Hooks::Voice::SendVoicePacket>(new CallHook("SendVoicePacket", Pointers.SendVoicePacket, Hooks::Voice::SendVoicePacket));
-		BaseHook::Add<Hooks::Voice::WriteVoiceInfoData>(new DetourHook("WriteVoiceInfoData", Pointers.WriteVoiceInfoData, Hooks::Voice::WriteVoiceInfoData));
+			BaseHook::Add<Hooks::Voice::EnumerateAudioDevices>(new DetourHook("EnumerateAudioDevices", Pointers.EnumerateAudioDevices, Hooks::Voice::EnumerateAudioDevices));
+			BaseHook::Add<Hooks::Voice::DirectSoundCaptureCreate>(new DetourHook("DirectSoundCaptureCreate", Pointers.DirectSoundCaptureCreate, Hooks::Voice::DirectSoundCaptureCreate));
+			BaseHook::Add<Hooks::Voice::SendVoicePacket>(new CallHook("SendVoicePacket", Pointers.SendVoicePacket, Hooks::Voice::SendVoicePacket));
+			BaseHook::Add<Hooks::Voice::WriteVoiceInfoData>(new DetourHook("WriteVoiceInfoData", Pointers.WriteVoiceInfoData, Hooks::Voice::WriteVoiceInfoData));
+		}
 
 		BaseHook::Add<Hooks::Misc::ThrowFatalError>(new DetourHook("ThrowFatalError", Pointers.ThrowFatalError, Hooks::Misc::ThrowFatalError));
 		BaseHook::Add<Hooks::Misc::IsAnimSceneInScope>(new DetourHook("IsAnimSceneInScope", Pointers.IsAnimSceneInScope, Hooks::Misc::IsAnimSceneInScope));
@@ -92,26 +97,31 @@ namespace YimMenu
 		BaseHook::Add<Hooks::Misc::CheckConditionIsMale>(new DetourHook("CheckConditionIsMale", Pointers.CheckConditionIsMale, Hooks::Misc::CheckConditionIsMale));
 		BaseHook::Add<Hooks::Misc::CheckConditionIsFemale>(new DetourHook("CheckConditionIsFemale", Pointers.CheckConditionIsFemale, Hooks::Misc::CheckConditionIsFemale));
 
-		BaseHook::Add<Hooks::Info::NetworkRequest>(new DetourHook("NetworkReqeust", Pointers.NetworkRequest, Hooks::Info::NetworkRequest));
+		if (TestFlag("no-net"))
+			LOG(WARNING) << "testflags: not hooking online code";
+		else
+		{
+			BaseHook::Add<Hooks::Info::NetworkRequest>(new DetourHook("NetworkReqeust", Pointers.NetworkRequest, Hooks::Info::NetworkRequest));
 
-		BaseHook::Add<Hooks::Info::PlayerHasJoined>(new DetourHook("PlayerHasJoined", Pointers.PlayerHasJoined, Hooks::Info::PlayerHasJoined));
-		BaseHook::Add<Hooks::Info::PlayerHasLeft>(new DetourHook("PlayerHasLeft", Pointers.PlayerHasLeft, Hooks::Info::PlayerHasLeft));
+			BaseHook::Add<Hooks::Info::PlayerHasJoined>(new DetourHook("PlayerHasJoined", Pointers.PlayerHasJoined, Hooks::Info::PlayerHasJoined));
+			BaseHook::Add<Hooks::Info::PlayerHasLeft>(new DetourHook("PlayerHasLeft", Pointers.PlayerHasLeft, Hooks::Info::PlayerHasLeft));
 
-		BaseHook::Add<Hooks::Info::HandleSessionEvent>(new DetourHook("HandleSessionEvent", Pointers.HandleSessionEvent, Hooks::Info::HandleSessionEvent));
+			BaseHook::Add<Hooks::Info::HandleSessionEvent>(new DetourHook("HandleSessionEvent", Pointers.HandleSessionEvent, Hooks::Info::HandleSessionEvent));
 
-		BaseHook::Add<Hooks::Spoofing::SendNetInfoToLobby>(new DetourHook("SendNetInfoToLobby", Pointers.SendNetInfoToLobby, Hooks::Spoofing::SendNetInfoToLobby));
-		BaseHook::Add<Hooks::Spoofing::WriteVPMData>(new DetourHook("WriteVehicleProximityMigrationData", Pointers.WriteVPMData, Hooks::Spoofing::WriteVPMData));
-		BaseHook::Add<Hooks::Spoofing::GetDiscriminator>(new DetourHook("GetDiscriminator", Pointers.GetDiscriminator, Hooks::Spoofing::GetDiscriminator));
-		BaseHook::Add<Hooks::Spoofing::WriteNodeData>(new DetourHook("WriteNodeData", Pointers.WriteNodeData, Hooks::Spoofing::WriteNodeData));
+			BaseHook::Add<Hooks::Spoofing::SendNetInfoToLobby>(new DetourHook("SendNetInfoToLobby", Pointers.SendNetInfoToLobby, Hooks::Spoofing::SendNetInfoToLobby));
+			BaseHook::Add<Hooks::Spoofing::WriteVPMData>(new DetourHook("WriteVehicleProximityMigrationData", Pointers.WriteVPMData, Hooks::Spoofing::WriteVPMData));
+			BaseHook::Add<Hooks::Spoofing::GetDiscriminator>(new DetourHook("GetDiscriminator", Pointers.GetDiscriminator, Hooks::Spoofing::GetDiscriminator));
+			BaseHook::Add<Hooks::Spoofing::WriteNodeData>(new DetourHook("WriteNodeData", Pointers.WriteNodeData, Hooks::Spoofing::WriteNodeData));
 
-		BaseHook::Add<Hooks::Toxic::BroadcastNetArray>(new DetourHook("BroadcastNetArray", Pointers.BroadcastNetArray, Hooks::Toxic::BroadcastNetArray));
-		BaseHook::Add<Hooks::Toxic::WriteSyncTree>(new DetourHook("WriteSyncTree", Pointers.WriteSyncTree, Hooks::Toxic::WriteSyncTree));
-		BaseHook::Add<Hooks::Toxic::ShouldUseNodeCache>(new DetourHook("ShouldUseNodeCache", Pointers.ShouldUseNodeCache, Hooks::Toxic::ShouldUseNodeCache));
-		BaseHook::Add<Hooks::Toxic::IsNodeInScope>(new DetourHook("IsNodeInScope", Pointers.IsNodeInScope, Hooks::Toxic::IsNodeInScope));
-		BaseHook::Add<Hooks::Toxic::SetTreeTargetObject>(new DetourHook("SetTreeTargetObject", Pointers.SetTreeTargetObject, Hooks::Toxic::SetTreeTargetObject));
+			BaseHook::Add<Hooks::Toxic::BroadcastNetArray>(new DetourHook("BroadcastNetArray", Pointers.BroadcastNetArray, Hooks::Toxic::BroadcastNetArray));
+			BaseHook::Add<Hooks::Toxic::WriteSyncTree>(new DetourHook("WriteSyncTree", Pointers.WriteSyncTree, Hooks::Toxic::WriteSyncTree));
+			BaseHook::Add<Hooks::Toxic::ShouldUseNodeCache>(new DetourHook("ShouldUseNodeCache", Pointers.ShouldUseNodeCache, Hooks::Toxic::ShouldUseNodeCache));
+			BaseHook::Add<Hooks::Toxic::IsNodeInScope>(new DetourHook("IsNodeInScope", Pointers.IsNodeInScope, Hooks::Toxic::IsNodeInScope));
+			BaseHook::Add<Hooks::Toxic::SetTreeTargetObject>(new DetourHook("SetTreeTargetObject", Pointers.SetTreeTargetObject, Hooks::Toxic::SetTreeTargetObject));
 
 	
-		BaseHook::Add<Hooks::Toxic::SerializeIceSessionOfferRequest>(new DetourHook("SerializeIceSessionOfferRequest", Pointers.SerializeIceSessionOfferRequest, Hooks::Toxic::SerializeIceSessionOfferRequest));
+			BaseHook::Add<Hooks::Toxic::SerializeIceSessionOfferRequest>(new DetourHook("SerializeIceSessionOfferRequest", Pointers.SerializeIceSessionOfferRequest, Hooks::Toxic::SerializeIceSessionOfferRequest));
+		}
 
 		if (!BaseHook::EnableAll())
 		{
